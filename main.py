fcc4264a2bae8a0579ac518a859f8bd9ad9e38f0
pranav-new-laptop <pranavmehe14@gmail.com>
2024-01-08 09:27:32 +0530

    Added config.json and made neccessary changes in main.py

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-FOLDER_ID="1T3RiNpcYS-vbtSa_AN7z_ZlQbiZtLJfj"
+import json
+
+
+with open("config.json", "r") as file:
+    parameters = json.load(file)['params']
+
+FOLDER_ID= parameters['FOLDER_ID']
+
+
 # Function to write data to Google Sheet
 def append_to_google_sheet(name, email,events,link):
     scope = ['https://www.googleapis.com/auth/drive.file',
@@ -13,23 +21,19 @@ def append_to_google_sheet(name, email,events,link):
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/drive.metadata'
   ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("litforms.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(parameters['CREDS_FILE'], scope)
     client = gspread.authorize(creds)
 
     # Replace 'Sheet Name' with your actual+ sheet name
-    sheet = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("Sheet1")
+    sheet = client.open_by_key(parameters['G_SHEET_ID']).worksheet(parameters['SHEET_NAME'])
     sheet.append_row([name, email,events,link])
     st.success("Event Registration completed successfully!")
-
-# Path to your credentials JSON file
-CREDENTIALS_FILE = 'litforms.json'  # Replace with your credentials file path
-
 
 # Function to upload file to Google Drive
 def upload_to_drive(file_path, file_name, mime_type,folder_id):
     # Load credentials from the credentials file
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("litforms.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(parameters['CREDS_FILE'], scope)
 
     # Authorize the API client
     service = build('drive', 'v3', credentials=creds)
@@ -67,18 +71,6 @@ name = st.text_input("Enter your name")
 email = st.text_input("Enter your email")
 # uploaded_file = st.file_uploader("Choose a file")
 
-costs = {
-    'Innovatia': 10,
-    'DPL': 20,
-    'Code of Lies': 30,
-    'Ctrl-Alt-Elite': 40,
-    'MUN' : 50,
-    'Brainiac': 60,
-    'Gamestorm': 70,
-    'Treasure Trove': 80,
-    'PromptAI': 90,
-    '11hr Hackathon': 100
-}
 
 # Empty dictionary to store the selected checkboxes and their costs
 selected_items = {}
@@ -87,7 +79,7 @@ selected_items = {}
 st.title("Events in ACUNETIX 11.0")
 
 # Iterate through the dictionary to create checkboxes
-for label, cost in costs.items():
+for label, cost in parameters['events'].items():
     checkbox_state = st.checkbox(label)
     if checkbox_state:
         selected_items[label] = cost
@@ -97,7 +89,10 @@ total_cost = sum(selected_items.values())
 events = ",".join(list(selected_items.keys()))
 # Display the total cost
 # st.write(f"Total Cost: {total_cost}")
-redirect_url = f"upi://pay?pa=pranavmehe14@okicici&pn=Pranav&cu=INR&am={total_cost}"
+
+Rid = "AAAA0000"                                      #TODO:Fetch Ids from google sheet
+
+redirect_url = f"upi://pay?pa={parameters['UPI_ID']}&pn={parameters['pn']}&cu=INR&am={total_cost}&tn={parameters['tn']+str(Rid)}"
 # Create the button-like link with specified styles
 button_styles = """
     <style>
